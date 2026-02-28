bdfb15c5a829a7d158c9007d17452f995d2a8e23
Shahzadforyou <zaeimshahzad@gmail.com>
2026-02-28 16:42:23 +0500

    improving code

--- palindrome.py
+++ palindrome.py
@@ -5,24 +5,36 @@ class Solution(object):
         :rtype: int
         """
         
-        if(x <0):
-            sign = -1
-        else:
-            sign = +1
-        x = abs(x)
+#         if(x <0):
+#             sign = -1
+#         else:
+#             sign = +1
+#         x = abs(x)
 
-        original = x
-        reverse = 0
-        while x > 0:
-            last_digit = x%10
-            reverse = reverse *10 + last_digit
-            x = x//10
-        reverse = reverse*sign
-        if(reverse < -2 ** 31 or reverse > 2 ** 31):
-            return 0
-        else:
-            return reverse
+#         original = x
+#         reverse = 0
+#         while x > 0:
+#             last_digit = x%10
+#             reverse = reverse *10 + last_digit
+#             x = x//10
+#         reverse = reverse*sign
+#         if(reverse < -2 ** 31 or reverse > 2 ** 31):
+#             return 0
+#         else:
+#             return reverse
         
 
+# s = Solution()
+# print(s.reverse(-1331))
+    def isPalindrome(self, x):
+        reverse = 0
+        remaining = abs(x)
+        limit = 2**31
+        if(x< -limit or x > limit-1):
+            return 0
+        while remaining:
+            reverse = reverse*10 + remaining%10
+            remaining = remaining//10
+        return -reverse if x < 0 else reverse
 s = Solution()
-print(s.reverse(-1331))
+print(s.isPalindrome(121))
