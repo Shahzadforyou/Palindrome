eb4a9d7bfc95194daf3eb9abcb1fc92fe1695216
Shahzadforyou <zaeimshahzad@gmail.com>
2026-02-28 16:01:15 +0500

    changing requirements for negitive and positive integer and also if number exceeds 32 bits

--- palindrome.py
+++ palindrome.py
@@ -4,16 +4,23 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        self.original = x
-        self.reverse = 0
-        while x > 0:
-            self.last_digit = x%10
-            self.reverse = self.reverse *10 + self.last_digit
-            x = x//10
-        if(self.reverse == self.original):
-            return True
+        
+        if(x <0):
+            sign = -1
         else:
-            return False
+            sign = +1
+        x = abs(x)
+
+        original = x
+        reverse = 0
+        while x > 0:
+            last_digit = x%10
+            reverse = reverse *10 + last_digit
+            x = x//10
+        if(reverse < -2 ** 31 or reverse > 2 ** 31):
+            return 0
+        else:
+            return reverse
         
 
 s = Solution()
