44753f79c76069710ecb8ccf003698b65b849970
Shahzadforyou <zaeimshahzad@gmail.com>
2026-03-01 22:17:29 +0500

    two doing same work one is faster then other

--- palindrome.py
+++ palindrome.py
@@ -1,31 +1,34 @@
-class Solution(object):
+class Solutions(object):
     def reverse(self, x):
         """
         :type x: int
         :rtype: int
         """
         
-#         if(x <0):
-#             sign = -1
-#         else:
-#             sign = +1
-#         x = abs(x)
+        if(x <0):
+            sign = -1
+        else:
+            sign = +1
+        x = abs(x)
 
-#         original = x
-#         reverse = 0
-#         while x > 0:
-#             last_digit = x%10
-#             reverse = reverse *10 + last_digit
-#             x = x//10
-#         reverse = reverse*sign
-#         if(reverse < -2 ** 31 or reverse > 2 ** 31):
-#             return 0
-#         else:
-#             return reverse
+        original = x
+        reverse = 0
+        while x > 0:
+            last_digit = x%10
+            reverse = reverse *10 + last_digit
+            x = x//10
+        reverse = reverse*sign
+        if(reverse < -2 ** 31 or reverse > 2 ** 31):
+            return 0
+        else:
+            return reverse
         
 
-# s = Solution()
-# print(s.reverse(-1331))
+s = Solutions()
+print(s.reverse(-1331))
+
+
+
 class Solution(object):
     def isPalindrome(self, x):
         reverse = 0
